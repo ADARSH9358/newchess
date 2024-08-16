3df0f1a9a6948a229ddbe963f11a0e47ed7f635d
Adarsh kumar <122091788+ADARSH9358@users.noreply.github.com>
2024-08-17 04:45:10 +0530

    ad

---
+++ index.js
@@ -0,0 +1,279 @@
+
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+const path = require('path');
+
+const fs = require('fs');
+
+// for logFile
+function logToFile(message) {
+    const logFilePath = path.join(__dirname, 'user_activity.log');
+    const timestamp = new Date().toISOString();
+    const logMessage = `${timestamp} - ${message}\n`;
+
+    fs.appendFile(logFilePath, logMessage, (err) => {
+        if (err) {
+            console.error('Failed to write to log file:', err);
+        }
+    });
+}
+
+
+
+// Serve static files from the "public" directory
+
+
+
+app.use(express.static(path.join(__dirname)));
+
+// Serve index.html on the root path
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+let gameState = {
+    rooms: {} // Each room will have its own game state
+};
+
+function initializeBoard() {
+    let board = [];
+    // create a 2d array
+    for (let i = 0; i < 8; i++) {
+        board[i] = new Array(8).fill(null);
+    }
+    const pieces = ['rook', 'knight', 'bishop', 'queen', 'king', 'bishop', 'knight', 'rook'];
+    for (let i = 0; i < 8; i++) {
+        board[0][i] = { type: pieces[i], color: 'white' };
+        board[1][i] = { type: 'pawn', color: 'white' };
+        board[6][i] = { type: 'pawn', color: 'black' };
+        board[7][i] = { type: pieces[i], color: 'black' };
+    }
+
+    return board;
+}
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+    logToFile(`User connected: ${socket.id}`);
+    socket.on('joinRoom', ({ name, room }) => {
+        socket.join(room);
+        console.log(`${name} joined room ${room}`);
+
+        if (!gameState.rooms[room]) {
+            gameState.rooms[room] = {
+                board: initializeBoard(),
+                turn: 'white',
+                players: {},
+                winner: null
+            };
+        }
+        let roomState = gameState.rooms[room];
+
+        if (!roomState.players.white) {
+            roomState.players.white = { id: socket.id, name: name };
+            socket.emit('playerColor', 'white');
+            logToFile(`User ${name} assigned color: white`);
+        } else if (!roomState.players.black) {
+            roomState.players.black = { id: socket.id, name: name };
+            socket.emit('playerColor', 'black');
+            logToFile(`User ${name} assigned color: black`);
+            // Notify both players of each other's names
+            io.to(roomState.players.white.id).emit('opponentName', roomState.players.black.name);
+            io.to(roomState.players.black.id).emit('opponentName', roomState.players.white.name);
+        } else {
+            const mess="Sorry, room is full.Please try other room";
+            socket.emit('roomFull', mess);
+        }
+
+        socket.emit('initGame', roomState);
+        socket.on('offer', (offer) => {
+            socket.broadcast.emit('offer', offer);
+        });
+
+        socket.on('answer', (answer) => {
+            socket.broadcast.emit('answer', answer);
+        });
+
+        socket.on('candidate', (candidate) => {
+            socket.broadcast.emit('candidate', candidate);
+        });
+
+        socket.on('movePiece', (move) => {
+            if (isValidMove(move, roomState, socket.id)) {
+                roomState = updateGameState(roomState, move);
+                io.to(room).emit('updateBoard', roomState);
+            } else {
+                socket.emit('invalidMove', { message: 'Invalid move or not your turn' });
+            }
+        });
+
+        socket.on('getPossibleMoves', ({ piece, position }) => {
+            const possibleMoves = calculatePossibleMoves(piece, position, roomState, socket.id);
+            // console.log("possible ", possibleMoves);
+            socket.emit('possibleMoves', possibleMoves);
+        });
+
+        socket.on('disconnect', () => {
+            console.log(`${name} disconnected from room ${room}`);
+            if (roomState.players.white && roomState.players.white.id === socket.id) {
+                delete roomState.players.white;
+            } else if (roomState.players.black && roomState.players.black.id === socket.id) {
+                delete roomState.players.black;
+            }
+
+            // If both players are disconnected, you may want to clear the room state
+            if (!roomState.players.white && !roomState.players.black) {
+                delete gameState.rooms[room];
+            }
+        });
+    });
+});
+function calculatePossibleMoves(piece, from, gameState,id) {
+    let board=gameState.board
+    const moves = [];
+    for (let row = 0; row < 8; row++) {
+        for (let col = 0; col < 8; col++) {
+            const to = { row, col };
+
+            if (isValidMove({ from, to }, { ...gameState, board }, id)) {
+                moves.push(to);
+            }
+        }
+    }
+    return moves;
+}
+function isValidMove(move, gameState, playerId) {
+    const { from, to } = move;
+    const piece = gameState.board[from.row][from.col];
+    
+    if (!piece || piece.color !== gameState.turn) {
+        // console.log("1f")
+        return false;
+    }
+    if (gameState.turn === 'white' && gameState.players.white.id !== playerId) {
+        //  console.log("2f") ;
+         return false;
+        }
+    if (gameState.turn === 'black' && gameState.players.black.id !== playerId){  return false;}
+
+    const target = gameState.board[to.row][to.col];
+    if (target && target.color === piece.color) { return false;}
+
+    switch (piece.type) {
+        case 'pawn':
+            return isValidPawnMove(from, to, piece.color, gameState.board);
+        case 'rook':
+            return isValidRookMove(from, to, gameState.board);
+        case 'knight':
+            return isValidKnightMove(from, to);
+        case 'bishop':
+            return isValidBishopMove(from, to, gameState.board);
+        case 'queen':
+            return isValidQueenMove(from, to, gameState.board);
+        case 'king':
+            return isValidKingMove(from, to, gameState.board);
+        default:
+            return false;
+    }
+}
+
+function isValidPawnMove(from, to, color, board) {
+    const direction = color === 'white' ? 1 : -1;
+    const startRow = color === 'white' ? 1 : 6;
+
+    if (to.row === from.row + direction && to.col === from.col && !board[to.row][to.col]) {
+        return true;
+    }
+
+    if (from.row === startRow && to.row === from.row + 2 * direction && to.col === from.col && !board[to.row][to.col] && !board[from.row + direction][from.col]) {
+        return true;
+    }
+
+    if (to.row === from.row + direction && Math.abs(to.col - from.col) === 1 && board[to.row][to.col] && board[to.row][to.col].color !== color) {
+        return true;
+    }
+    // console.log("5f")
+    return false;
+}
+
+function isValidRookMove(from, to, board) {
+    if (from.row !== to.row && from.col !== to.col) return false;
+    
+    if (from.row === to.row) {
+        const [start, end] = from.col < to.col ? [from.col, to.col] : [to.col, from.col];
+        for (let i = start + 1; i < end; i++) {
+            if (board[from.row][i]) return false;
+        }
+    } else {
+        const [start, end] = from.row < to.row ? [from.row, to.row] : [to.row, from.row];
+        for (let i = start + 1; i < end; i++) {
+            if (board[i][from.col]) return false;
+        }
+    }
+    
+    return true;
+}
+
+function isValidKnightMove(from, to) {
+    const rowDiff = Math.abs(from.row - to.row);
+    const colDiff = Math.abs(from.col - to.col);
+    return (rowDiff === 2 && colDiff === 1) || (rowDiff === 1 && colDiff === 2);
+}
+
+function isValidBishopMove(from, to, board) {
+    const rowDiff = Math.abs(from.row - to.row);
+    const colDiff = Math.abs(from.col - to.col);
+    if (rowDiff !== colDiff) return false;
+    
+    const rowStep = from.row < to.row ? 1 : -1;
+    const colStep = from.col < to.col ? 1 : -1;
+    
+    for (let i = 1; i < rowDiff; i++) {
+        if (board[from.row + i * rowStep][from.col + i * colStep]) return false;
+    }
+    
+    return true;
+}
+
+function isValidQueenMove(from, to, board) {
+    return isValidRookMove(from, to, board) || isValidBishopMove(from, to, board);
+}
+
+function isValidKingMove(from, to, board) {
+    const rowDiff = Math.abs(from.row - to.row);
+    const colDiff = Math.abs(from.col - to.col);
+    return rowDiff <= 1 && colDiff <= 1;
+}
+
+
+function updateGameState(gameState, move) {
+    const { from, to } = move;
+    const piece = gameState.board[from.row][from.col];
+    const capturedPiece = gameState.board[to.row][to.col];
+
+    gameState.board[to.row][to.col] = piece;
+    gameState.board[from.row][from.col] = null;
+    gameState.turn = gameState.turn === 'white' ? 'black' : 'white';
+
+    if (capturedPiece && capturedPiece.type === 'king') {
+        gameState.winner = piece.color;
+        io.emit('gameOver', { winner: piece.color });
+    }
+
+    return gameState;
+}
+
+let PORT=3000;
+server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,260 @@
+const socket = io();
+let board = [];
+let playerColor = null;
+let selectedPiece = null;
+let possibleMoves = [];
+let gameState = {
+    board: [],
+    turn: 'white',
+    winner: null
+};
+let userName;
+let userRoom;
+let localStream;
+let peerConnection;
+const localVideo = document.getElementById('localVideo');
+const remoteVideo = document.getElementById('remoteVideo');
+const callButton = document.getElementById('callButton');
+
+const config = {
+    iceServers: [
+        {
+            urls: "turn:relay1.expressturn.com:3478",
+            username: "efD08GKZ1QV5X7KPMN",
+            credential: "2vS3rl5sPO0tp3P9",
+        }
+    ]
+};
+
+callButton.addEventListener('click', startCall);
+
+
+socket.on('connect', () => {
+    console.log('Connected to server');
+});
+
+// Handle the login form submission (only on the login page)
+function handleLogin(event) {
+    event.preventDefault();
+    
+    userName = document.getElementById("name").value;
+    userRoom = document.getElementById("email").value;
+
+    if (userName) {
+        // Store user data in localStorage
+        localStorage.setItem("userName", userName);
+        localStorage.setItem("userRoom", userRoom);
+
+        const loginSection = document.getElementById('loginSection');
+        const gameSection = document.getElementById('gameSection');
+        if (loginSection && gameSection) {
+            // Hide login section and show game section
+            loginSection.style.display = 'none';
+            gameSection.style.display = 'block';
+
+            // Emit the joinRoom event
+            socket.emit('joinRoom', { name: userName, room: userRoom });
+        } else {
+            console.error("Login or game section not found");
+        }
+    } else {
+        console.error("Please enter a name");
+    }
+
+    return false; // Prevent default form submission behavior
+}
+socket.on('playerColor', (color) => {
+    playerColor = color;
+    // if (color === "white") {
+    //     document.getElementById("whosTurn").innerHTML = `<div>It is ${userName}'s turn</div>`;
+    // }
+    // console.log('You are playing as', color);
+});
+
+// socket.on('initGame', (gameState) => {
+//     // Example: Initialize game state and render the board
+//     console.log('Game initialized', gameState);
+// });
+
+// web rtc
+
+
+callButton.addEventListener('click', startCall);
+// hangUpButton.addEventListener('click', hangUp);
+
+async function startCall() {
+    localStream = await navigator.mediaDevices.getUserMedia({ audio: true,video: true });
+    localVideo.srcObject = localStream;
+    // localVideo.style.display = "none"; 
+    peerConnection = new RTCPeerConnection(config);
+    peerConnection.addStream(localStream);
+
+    peerConnection.onicecandidate = (event) => {
+        if (event.candidate) {
+            socket.emit('candidate', event.candidate);
+        }
+    };
+
+    peerConnection.onaddstream = (event) => {
+        // const remoteAudio = new Audio();
+        // remoteAudio.srcObject = event.stream;
+        // remoteAudio.play();
+        remoteVideo.srcObject = event.stream;
+    };
+
+    const offer = await peerConnection.createOffer();
+    await peerConnection.setLocalDescription(offer);
+    socket.emit('offer', offer);
+}
+
+socket.on('offer', async (offer) => {
+    if (!peerConnection) {
+        startCall();
+    }
+    await peerConnection.setRemoteDescription(new RTCSessionDescription(offer));
+    const answer = await peerConnection.createAnswer();
+    await peerConnection.setLocalDescription(answer);
+    socket.emit('answer', answer);
+});
+
+socket.on('answer', async (answer) => {
+    await peerConnection.setRemoteDescription(new RTCSessionDescription(answer));
+});
+
+socket.on('candidate', (candidate) => {
+    const iceCandidate = new RTCIceCandidate(candidate);
+    peerConnection.addIceCandidate(iceCandidate);
+});
+
+function hangUp() {
+    peerConnection.close();
+    localStream.getTracks().forEach(track => track.stop());
+    peerConnection = null;
+}
+
+
+socket.on('initGame', (gameState) => {
+    board = gameState.board;
+    renderBoard(board);
+
+});
+// io.to(roomState.players.white.id).emit('opponentName', roomState.players.black.name);
+// 
+
+socket.on("roomFull",(mess)=>{
+    alert(mess);
+    return ;
+})
+socket.on('updateBoard', (gameState) => {
+    board = gameState.board;
+    // let turnDiv=  document.querySelector('#turn')
+    // document.getElementById("whosTurn").style.display = "block"
+    let color=gameState.turn;
+    console.log(color);
+    let ans=gameState.players[color].name;
+    console.log(ans);
+    document.getElementById("whosTurn").innerHTML=`<div>It is ${ans} turn</div>`;
+    
+    possibleMoves = [];
+    renderBoard(board);
+  
+});
+
+socket.on('invalidMove', (data) => {
+    alert(data.message);
+});
+socket.on('gameOver', (data) => {
+    gameState.winner = data.winner;
+    // document.querySelector('.win').innerHTML=`<h1>the winner is ${data.winner}</h1>`;
+    alert(`Game over! The winner is ${data.winner}`);
+
+  });
+
+
+  socket.on('opponentName', (opponentName) => {
+
+    document.querySelector(".player-name").innerText=opponentName;
+    // window.location.href = 'chess.html';
+
+    // opponentDiv.classList.add('opponenthighlight');
+});
+
+function renderBoard(board) {
+    const chessboard = document.querySelector('.chessboard');
+    chessboard.innerHTML = ''; // Clear the chessboard before rendering
+
+    for (let row = 0; row < 8; row++) {
+        for (let col = 0; col < 8; col++) {
+            // Create a new div for each cell
+            const cell = document.createElement('div');
+            cell.id = `${col + 1}_${8 - row}`;
+            cell.className = `gamecell ${(row + col) % 2 === 0 ? 'white' : 'grey'}`;
+
+            if (board[row][col]) {
+                // Add piece to the cell
+                cell.innerHTML = `<span class="${board[row][col].type}">${getPieceSymbol(board[row][col])}</span>`;
+                if (!gameState.winner) {
+                    if (board[row][col].color === playerColor) {
+                        cell.addEventListener('click', () => selectPiece(row, col));
+                    } else {
+                        cell.addEventListener('click', () => movePiece(row, col));
+                    }
+                }
+            } else {
+                // Add click event for empty cell if the game is not over
+                if (!gameState.winner) {
+                    cell.addEventListener('click', () => movePiece(row, col));
+                }
+            }
+
+            // Highlight possible moves
+            if (possibleMoves.some(move => move.row === row && move.col === col)) {
+                cell.classList.add('highlight');
+            }
+
+            // Append the cell to the chessboard
+            chessboard.appendChild(cell);
+        }
+    }
+}
+
+
+
+function getPieceSymbol(piece) {
+    switch (piece.type) {
+        case 'pawn': return piece.color === 'white' ? '♙' : '♟';
+        case 'rook': return piece.color === 'white' ? '♖' : '♜';
+        case 'knight': return piece.color === 'white' ? '♘' : '♞';
+        case 'bishop': return piece.color === 'white' ? '♗' : '♝';
+        case 'queen': return piece.color === 'white' ? '♕' : '♛';
+        case 'king': return piece.color === 'white' ? '♔' : '♚';
+        default: return '';
+    }
+}
+
+function selectPiece(row, col) {
+    selectedPiece = { row, col };
+    const piece = board[row][col];
+    if (piece && piece.color === playerColor) {
+        console.log(piece);
+        console.log(row,col);
+        socket.emit('getPossibleMoves', { piece, position: selectedPiece });
+    }
+}
+// to highlight it
+socket.on('possibleMoves', (moves) => {
+    possibleMoves = moves;
+    renderBoard(board);
+});
+
+function movePiece(row, col) {
+    if (selectedPiece) {
+        const move = {
+            from: selectedPiece,
+            to: { row, col }
+        };
+        socket.emit('movePiece', move);
+        selectedPiece = null;
+        possibleMoves = [];
+    }
+}
